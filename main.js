7416460b1b37b219cfe76050283af5ec37a9df85
Evandro Sousa <66460660+vandynM@users.noreply.github.com>
2022-05-29 16:04:00 -0300

    Add files via upload
    Atualização final, design responsivo concluido!

--- main.js
+++ main.js
@@ -4,6 +4,37 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  // linha alvo
+  const targetLine = scrollY + innerHeight / 2
+
+  const sectionTop = section.offsetTop // o tempo da seção
+  const sectionHeight = section.offsetHeight // a altura da seção
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop // o topo da seção chegou ou ultrapassou a linha alvo
+
+  //verificar se a base está abaixo da linha alvo
+
+  const sectionEndsAt = sectionTop + sectionHeight
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+  // limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetline
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
 
 function showNavOnScroll() {
